0c5e4d8ca3e5690281230fcdd775cb967b3eef7d
sagar-chandani <spc1194@gmail.com>
2020-10-13 12:13:47 -0500

    solved issorted challenge

--- data_structures_in_javascript_master_the _fundamentals/01-arrays/06-is-sorted-challenge.js
+++ data_structures_in_javascript_master_the _fundamentals/01-arrays/06-is-sorted-challenge.js
@@ -10,4 +10,21 @@
 // [1, 5, 2] => false
 function isSorted(arr) {
 
+    let isAscending = true;
+    let isDescending = true
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] > arr[i]) {
+            isAscending = false;
+        } else if ( arr[i-1] < arr[i]){
+             isDescending = false;
+        }
+    }
+    return isAscending || isDescending;
 }
+
+let cl = console.log;
+cl(isSorted([]));
+cl(isSorted([39, 42]));
+cl(isSorted([1, 2, 3, 4, 5]));
+cl(isSorted([5, 4, 3, 2, 1]));
+cl(isSorted([1,5,2]));
