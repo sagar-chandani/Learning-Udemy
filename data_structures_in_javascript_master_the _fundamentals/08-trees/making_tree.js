9661f3aad492707d8593cf3cc95b01eb78082d12
sagar-chandani <spc1194@gmail.com>
2020-10-15 19:20:27 -0500

    made tree again

---
+++ data_structures_in_javascript_master_the _fundamentals/08-trees/making_tree.js
@@ -0,0 +1,44 @@
+class TreeNode{
+    constructor(data,left=null,right=null){
+        this.data=data;
+        this.left=left;
+        this.right=right;
+    }
+}
+
+class Tree{
+    constructor(){
+        this.root = null;
+    }
+
+    collect(){
+       return this._collect(this.root,[])
+    }
+
+    _collect(current,nodes){
+        if(current === null){
+            return nodes;
+        }
+        nodes.push(current.data);
+
+        this._collect(current.left,nodes);
+        this._collect(current.right,nodes);
+
+        return nodes;
+    }   
+}
+
+let n1 = new TreeNode(32);
+let n2 = new TreeNode(84);
+let n3 = new TreeNode(94);
+let n4 = new TreeNode(56);
+let n5 = new TreeNode(13);
+
+let tree = new Tree();
+tree.root = n1;
+n1.left = n2;
+n2.right = n3;
+n3.right = n4;
+n4.left = n5;
+
+console.log(tree.collect())
