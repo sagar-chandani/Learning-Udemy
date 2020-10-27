1f7fa0ed75fef4f166ddf700136670474547fa03
sagar-chandani <spc1194@gmail.com>
2020-10-26 22:55:47 -0500

    updated goldman sachs interview experience 10/26

--- data_structures_in_javascript_master_the _fundamentals/08-trees/goldmansachs_3rd_round.js
+++ data_structures_in_javascript_master_the _fundamentals/08-trees/goldmansachs_3rd_round.js
@@ -5,3 +5,5 @@ https://medium.com/swlh/algorithms-with-javascript-median-of-two-sorted-arrays-2
 // 2nd question : given that a dictionary of words and an input find the longest word possible from that unordered pair of input
 
 // I am excited to get the next round and finally start my career with goldman sachs
+
+// todays monday 10/26 -  no update from goldman sachs yet! waiting for next round
