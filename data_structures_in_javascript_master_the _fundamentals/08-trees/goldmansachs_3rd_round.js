387846a11ebea90683ce0b927b279f2c2a790c4b
sagar-chandani <spc1194@gmail.com>
2020-11-05 21:52:03 -0600

    goldman sachs final round

--- data_structures_in_javascript_master_the _fundamentals/08-trees/goldmansachs_3rd_round.js
+++ data_structures_in_javascript_master_the _fundamentals/08-trees/goldmansachs_3rd_round.js
@@ -39,7 +39,107 @@ console.log(reverseEachWord)
 function isInt(num) {
     return num % 1 === 0;
   }
-  
+  ------
   console.log(isInt(4)); // true
   console.log(isInt(12.2)); // false
   console.log(isInt(0.3)); // false
+
+
+  /*
+  Goldman sachs final round
+
+  30 mins 3 interrview, half an hour break and then 1 hour of system design interrview
+
+  1 round : string manipulation for anagrams = number of deletion required to make an anagram - optmimize it - could get to O(nm) with stack implementation
+
+  2 round : matrix find sum of a subsection : explained in O(mn) couldnt optimize further
+
+  3rd round : SQL common questions related to inner join outer join
+
+  4th round : design a parking lot
+  // 
+// Your previous Java content is preserved below:
+// 
+// 
+// /* 
+// Your previous Plain Text content is preserved below:
+// 
+// Design a parking lot system with OO concepts
+// 
+// Assumptions
+// 1. Multiple levels, each level with multiple spots
+// 2. 3 vehicle sizes in our universe (compact/two-wheelers, cars, trucks)
+// 3. Parking spots offered are compact, medium, and large
+// 4. Smaller vehicles can park in a larger spot
+// 
+// 
+// classes : 
+// 
+//  
+/*
+
+
+let vehicle_size = [S,M,L]
+let ParkingLot = [S,M,L]
+let parkingLotLevels= [1,2,3]
+let spotsOccupied = {}
+
+class vehicle{
+  constructor(){
+    this.id = id
+    this.size = size;
+  }  
+}
+// function Parking(vehicle){
+  
+//   for (let i = 0; i < parkingLotLevels.length - 1;i++){
+      
+//   }
+// }
+
+
+
+// L1: S,S,S,M,M,M,L,L,L
+// L2: S,S,S,S,S,M,M,M,M
+// L3: S,S,S,S,M,M,M,M,M
+
+class ParkingLot{
+  constructor(){
+    this.parkingLotLevels = parkingLotLevels;
+    this.parkingSpotSize = [S,M,L]
+    this.totalParkingSpots = [[S,S,S,M,M,M,L,L,L],
+                             [S,S,S,S,S,M,M,M,M],
+                             [S,S,S,S,M,M,M,M,M]]
+    this.spotsOccupied = [[0,1,0,0,1,1,0,0,0],
+                          [0,0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,1,0]]
+  }
+
+  function checkIfSpotAvailable(){
+    
+    if (spotsOccupied.length === totaPakringSpots. ){
+        return false;
+      }
+    else return true
+  }
+  
+  function parkingSpotSizeCompatibility(size){
+      for(let i = 0; i< totalParkingSpots.length - 1;i++){
+          for (let j = 0; j< totalparkingspots[i].length -1; j++){
+              if(spotsOccupied[i][j] == 0){
+                  if(size === totalParkingSpots[i][j]){
+                      
+                    spotsOccupied[i][j] = 1;
+                    return "you can park here"
+                  } 
+                
+              }
+          }
+      }
+  }
+  
+
+}
+   
+
+  */
