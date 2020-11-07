9ed807b02f7defe1f4656c5d278a14834cd655ee
sagar-chandani <spc1194@gmail.com>
2020-11-06 22:18:23 -0600

    corrected errors

--- data_structures_in_javascript_master_the _fundamentals/08-trees/goldmansachs_3rd_round.js
+++ data_structures_in_javascript_master_the _fundamentals/08-trees/goldmansachs_3rd_round.js
@@ -39,7 +39,6 @@ console.log(reverseEachWord)
 function isInt(num) {
     return num % 1 === 0;
   }
-  ------
   console.log(isInt(4)); // true
   console.log(isInt(12.2)); // false
   console.log(isInt(0.3)); // false
