f875e9e02c009a2df8932cc7121ac36971a934c0
sagar-chandani <spc1194@gmail.com>
2020-12-08 20:21:59 -0600

    random coding praqctice

--- data_structures_in_javascript_master_the _fundamentals/test.js
+++ data_structures_in_javascript_master_the _fundamentals/test.js
@@ -12,4 +12,68 @@ function container(arr, value){
 }
  
 const c = [12,84,55,37,48];
-console.log(container([12,84,55,37,48], 55))    
+// console.log(container([12,84,55,37,48], 55))    
+
+
+const g = () => {
+  var hey = this.name  
+  console.log(this.name)
+  return hey;
+}
+const p = {name : "hello"};
+g.call(this.p)
+
+const add = {
+
+  street : 'elm str',
+  state: 's',
+}
+
+const {street:s,state:d} = add;
+console.log("str",s)
+console.log("state",d)
+
+
+function calculate(arr,msg){
+  arr[1] = 150;
+  msg = 'inside'
+  console.log('arr === ',arr[0],arr[1])
+
+  console.log(arr[0]  + arr[1]);
+  console.log(msg)
+}
+
+const arr = [100]
+console.log('arr === ',arr)
+let  msg = 'outside'
+
+calculate(arr, msg)
+console.log('arr === ',arr[0],arr[1])
+
+console.log(arr[0]  + arr[1]);
+console.log(msg)
+
+console.log('=====================')
+
+const x = {};
+x['foo'] = 'bar'
+x.bar = {
+  'first': 100,
+  'second': 200
+}
+console.log(x.bar['first'] + x.bar['second'])
+
+console.log('=====================')
+
+var bar = 5;
+function foo(bar){
+  if(bar >= 5){
+    bar = 'zzz'
+  }else{
+    let bar = 'qux'
+  }
+  console.log(bar)
+}
+foo(2)
+foo(6)
+console.log(bar)
